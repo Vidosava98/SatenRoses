3a1f8d90b4edbb745c4427819ebbc7221ef180f4
Vidosava98 <arsic.vida@gmail.com>
2025-07-11 17:54:26 +0200

    Init project.
    Init project.

---
+++ web-shop/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <center>
+        <h1 className="mt-16 text-black font-bold text-2xl">Saten Roses</h1>
+      </center>
+    </div>
+  );
+}
