00e94f0aaf7dfa9d429dff9df891727a36892de7
Vidosava98 <arsic.vida@gmail.com>
2025-07-24 16:03:55 +0200

    Change footer. Delete  resource.

--- web-shop/src/app/_components/Footer.tsx
+++ web-shop/src/app/_components/Footer.tsx
@@ -19,15 +19,15 @@ function Footer() {
   ];
   return (
     <div
-      className="flex lg:flex-row md:flex-row flex-col h-64 mt-48 p-8 bg-(--foreground) flex-wrap"
+      className="flex flex-col w-96 lg:w-full lg:flex-row md:flex-row mt-48 p-16 lg:p-8 md:p-8 bg-(--foreground) flex-wrap"
       id="contact"
     >
       <img
-        className="absolute left-24 w-24 h-24 transform translate-y-1/2"
+        className="absolute left-8 lg:left-24 md:left-24 w-24 h-24 transform translate-y-1/2"
         src={"./data/rose.png"}
         alt="Rose"
       />
-      <div className="flex flex-col flex-wrap w-80 lg:ml-64 md:ml-64 text-left pl-32">
+      <div className="flex flex-col flex-wrap w-64 lg:ml-64 md:ml-64 text-left pl-32 mt-8">
         {navBarList.map((item, i) => (
           <a
             key={i}
@@ -38,13 +38,30 @@ function Footer() {
           </a>
         ))}
       </div>
-      <div className="flex flex-col w-80 lg:ml-16 md:ml-16">
+      <div className="flex flex-col w-80 lg:ml-16 md:ml-16 mt-8">
         <p className="text-white">Send me Mail</p>
         <a href="mailto:arsic.vida@gmail.com">
           <img
-            src="/gmail.svg"
+            src="/icons/gmail.svg"
             alt="gmail"
-            className="h-[2.80rem] w:[3.5rem] m-4"
+            className="h-[2.80rem] w:[3.5rem] m-4 bg-white"
+          />
+        </a>
+      </div>
+      <div className="flex flex-col w-80 lg:ml-16 md:ml-16 mt-8">
+        <p className="text-white">Follow me here</p>
+        <a href="https://www.facebook.com/satenske.ruze.616102">
+          <img
+            src="/icons/face.svg"
+            alt="gmail"
+            className="h-[2.80rem] w:[3.5rem] m-4 bg-white"
+          />
+        </a>
+        <a href="https://www.instagram.com/satenskeruze__nis/">
+          <img
+            src="/icons/instagram.svg"
+            alt="gmail"
+            className="h-[2.80rem] w:[3.5rem] m-4 bg-white"
           />
         </a>
       </div>
