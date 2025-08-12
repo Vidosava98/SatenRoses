577cfd4d3924f687160358091e49d80a4c002fe1
Vidosava98 <arsic.vida@gmail.com>
2025-08-13 00:00:51 +0200

    Make ui changes.

--- web-shop/src/app/_components/mostBuyProd.tsx
+++ web-shop/src/app/_components/mostBuyProd.tsx
@@ -16,10 +16,10 @@ function MostBuyProd() {
   const products = [product1, product2];
   return (
     <div className="md:m-24 lg:m-24 m-8" id="themost">
-      <p className="text-black text-2xl md:text-4xl lg:text-4xl font-semibold text-center my-16 lg:my-8 md:my-8">
+      <p className="text-black text-2xl md:text-4xl lg:text-4xl font-semibold text-center my-16 lg:mb-8 md:mb-8 lg:mt-32 md:mt-32">
         The Most Popular Products
       </p>
-      <div className="flex flex-wrap justify-center text-center">
+      <div className="flex flex-wrap justify-center text-center rounded-xl shadow-xl p-8 lg:pb-16 lg:gap-32 gap-8">
         {products.map((el, index) => (
           <Product {...el} key={index} />
         ))}
