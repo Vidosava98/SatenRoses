81242b86b0e0e0744ff7cb6c8d8897b024d563f4
Vidosava Arsić <arsic.vida@gmail.com>
2025-09-01 00:33:38 -0700

    change mostbuyprod card.

--- web-shop/src/app/_components/product.tsx
+++ web-shop/src/app/_components/product.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-type Props = { src: string; desc: string; price: string };
+type Props = { src: string; };
 
-function Product({ src, desc, price }: Props) {
+function Product({ src}: Props) {
   return (
-    <div className="flex row justify-center items-center gap-8 w-96 p-8 rounded-xl border-0 shadow-xl bg-amber-50">
+    <div className="flex row justify-center items-center gap-8 p-8 rounded-xl border-0 shadow-xl bg-amber-50">
       <img
         src={src}
         alt="Most Buying"
-        className="min-w-42 max-h-42 lg:w-64 lg:h-64 lg:max-h-64 lg:min-h-64 rounded-xl "
+        className="min-w-42 max-h-42 lg:w-64 lg:h-64 lg:max-h-64 lg:min-h-64 rounded-xl hover:scale-130"
       />
-      <div>
+      {/* <div>
         <p className="text-black text-2xl md:text-xl text-center">{desc}</p>
         <p className="text-black text-2xl md:text-xl font-semibold text-center bg-(--accent)">
           {price}
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
